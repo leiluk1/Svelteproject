bcd8c12df94de5f075d8c0764694a7510cc31c2d
leiluk1 <l.khaertdinova@innopolis.university>
2022-07-06 02:20:49 +0300

    Prettier and Eslint

--- src/hooks.ts
+++ src/hooks.ts
@@ -1,20 +1,20 @@
-import type { Handle } from '@sveltejs/kit';
-import * as cookie from 'cookie';
+import type { Handle } from "@sveltejs/kit";
+import * as cookie from "cookie";
 
 export const handle: Handle = async ({ event, resolve }) => {
-	const cookies = cookie.parse(event.request.headers.get('cookie') || '');
-	event.locals.userid = cookies['userid'] || crypto.randomUUID();
+	const cookies = cookie.parse(event.request.headers.get("cookie") || "");
+	event.locals.userid = cookies["userid"] || crypto.randomUUID();
 
 	const response = await resolve(event);
 
-	if (!cookies['userid']) {
+	if (!cookies["userid"]) {
 		// if this is the first time the user has visited this app,
 		// set a cookie so that we recognise them when they return
 		response.headers.set(
-			'set-cookie',
-			cookie.serialize('userid', event.locals.userid, {
-				path: '/',
-				httpOnly: true
+			"set-cookie",
+			cookie.serialize("userid", event.locals.userid, {
+				path: "/",
+				httpOnly: true,
 			})
 		);
 	}
